b1e037fb35a3591767526eef29a8fd297efce4fb
oluwaseun007 <preciousopal.od@gmail.com>
2023-05-22 15:16:08 +0100

    file updates

--- Building-Restful-APIs-using-Express/NodeJS-Tutorial-app.js
+++ Building-Restful-APIs-using-Express/NodeJS-Tutorial-app.js
@@ -1,6 +1,22 @@
-const express = require('expres');
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const port = 3000;
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/user/:name/:age/:sex', (req, res) => {
+    const {name, age, sex} = req.params;
+    const user = {name, age, sex};
+    res.json(user);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
 
